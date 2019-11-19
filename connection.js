f9ba2c9a8db9c3c1d52e9aabcb9102ceb0d02088
Shibata18 <douglas.shibata11@gmail.com>
2019-11-19 11:37:13 -0300

    CRUD básico

--- connection.js
+++ connection.js
@@ -16,6 +16,47 @@ const getCursos = (request, response) => {
         response.status(200).json(results.rows)
     })
 }
+//Ordenando a tabela cursos
+const getCursosOrder = (request, response) => {
+    pool.query('SELECT * FROM cursos order by nome_curso', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+// INserirndo um curso 
+const inserirCurso = function (req, res) {
+    const { id, nome, carga } = req.body;
+    pool.query(`INSERT INTO cursos VALUES ($1,$2,$3)`, [id, nome,carga], (error, results) => {
+        if (error) {
+            throw error
+        }
+        res.status(201).json(results.rows);
+    })
+}
+//Atualizando Curso 
+const atualizaCurso = function(req,res){
+    const { id, nome, carga } = req.body;
+    pool.query('UPDATE cursos SET nome_curso = ($2), carga_hor_max = ($3) where (id_cursos = ($1))',
+    [id, nome,carga], (error, results) => {
+        if (error) {
+            throw error
+        }
+        res.status(201).json(results.rows);
+    })
+}
+//Deletando Curso
+const deletaCurso = function(req,res){
+    const { id, nome, carga } = req.body;
+    pool.query('DELETE FROM cursos WHERE (nome_curso = ($2)) or (carga_hor_max = ($3)) or (id_cursos = ($1))',
+    [id, nome,carga], (error, results) => {
+        if (error) {
+            throw error
+        }
+        res.status(201).json(results.rows);
+    })
+}
 //Consultando a tabela usuario
 const getUsuario = (request, response) => {
     pool.query('SELECT * FROM usuarios', (error, results) => {
@@ -34,19 +75,23 @@ const getAtividade = (request, response) => {
         response.status(200).json(results.rows)
     })
 }
-const setAtividade = (req,res)=>{
+const setAtividade = (req, res) => {
     const { Semestre, atividade, horas } = req.body
-    pool.query(`INSERT INTO atividades VALUES ($1,$2,$3)`,[Semestre, atividade, horas],(error,results)=>{
-        if(error){
+    pool.query(`INSERT INTO atividades VALUES ($1,$2,$3)`, [Semestre, atividade, horas], (error, results) => {
+        if (error) {
             throw error
         }
         res.status(201).send(`Horas Adicionadas`);
     })
 }
 //Exportando a função
-module.exports={
+module.exports = {
     getCursos,
     getAtividade,
     getUsuario,
-    setAtividade
+    setAtividade,
+    getCursosOrder,
+    inserirCurso,
+    atualizaCurso,
+    deletaCurso
 }
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const app = express();
 const db = require('./connection');
+const app = express();
 const port = 3000;
 
 app.use(cors());
@@ -18,11 +18,17 @@ app.get('/', (request, response) => {
 });
 //Curso
 app.get('/cursos',db.getCursos);
+app.get('/cursosOrder',db.getCursosOrder);
+app.post('/cursos',db.inserirCurso);
+app.put('/cursos',db.atualizaCurso);
+app.delete('/cursos',db.deletaCurso);
 //Usuario
 app.get('/usuario',db.getUsuario);
 //Atividade
 app.get('/atividade',db.getAtividade);
 app.post('/atividade',db.setAtividade);
+
+
 app.listen(port, () => {
     console.log(`Rodando na Porta ${port}.`)
 })
